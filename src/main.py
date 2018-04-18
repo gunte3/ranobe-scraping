059873b10d9591d2466177a599b7bbfe39d13a5a
gunte3 <gunte1101@yahoo.co.jp>
2018-04-18 23:36:15 +0900

    update src/main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from bs4 import BeautifulSoup
 import urllib.request as req
 
+import pandas as pd
+import numpy as np
+
 url = "http://dengekibunko.jp/newreleases/"
 res = req.urlopen(url)
 soup = BeautifulSoup(res, "html.parser")
 
+book_table = pd.DataFrame(
+	columns = ['title', 'author1', 'author2', 'value', 'date', 'isbn']
+)
 # #new-anchor-01 > ul > li:nth-child(1) > h3 > a
 li_list = soup.select("#new-anchor-01 > ul > li")
 for li in li_list:
@@ -12,4 +18,19 @@ for li in li_list:
 	if a != None:
 		title = a.string
 		href = a.attrs["href"]
-		print(title + " (" + href + ")")
+		res_href = req.urlopen(href)
+		soup_href = BeautifulSoup(res_href, "html.parser")
+		author_info = soup_href.select('#author-info > li')
+		author1 = author_info[0].string
+		author2 = author_info[1].string
+		value = soup_href.select('#book-intro > li')[0].string
+		book_intro_2 = soup_href.select('#book-intro-2 > li')
+		date = book_intro_2[0].string
+		isbn = book_intro_2[1].string
+		tbl = pd.DataFrame(
+			[[title, author1, author2, value, date, isbn]],
+			columns = book_table.columns
+		)
+		book_table = book_table.append(tbl, ignore_index = True)
+print(book_table)
+book_table.to_csv("./log/dengeki-2018-04.csv")
